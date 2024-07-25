04b4fb7b9e2015270526af35f5c4116ce9cd0908
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-07-25 15:17:16 +0300

    second commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,5 @@
 class MainController < AboutController 
   def index
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,21 @@ Rails.application.routes.draw do
   # GET /about  .... index is the action down below
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
   root "main#index"
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
